d4dac4f3b6f1510963d42af4fce550c17c954555
Subhrajyoti Mahato <86191223+Subhrajyoti2608@users.noreply.github.com>
2023-08-13 12:58:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const app = express();
 const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-const {v4:uuidv4} = require("uuid")
-app.get("/",(req,res)=>{
-    res.redirect(`/${uuidv4()}`)
-})
-app.get("/:room",(req,res)=>{
-    res.render("index",{roomId:req.params.room})
-})
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+io.on("connection", (socket) => {
+    socket.on("message", (message) => {
+        io.emit("createMessage", message);
+    });
+});
+
 server.listen(3030);
